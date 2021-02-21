01366fd7985fd4ff595a947f15097106b90fc61c
SalauddinAyub <ayub35-265@diu.edu.bd>
2021-02-21 17:00:10 +0600

    product page updated

--- AppShop/urls.py
+++ AppShop/urls.py
@@ -5,5 +5,6 @@ app_name = 'AppShop'
 
 urlpatterns = [
     path('', views.Home.as_view(), name='home'),
+    path('product/<pk>/', views.ProductDetail.as_view(), name='product_detail'),
     
 ]
--- AppShop/views.py
+++ AppShop/views.py
@@ -6,9 +6,16 @@ from django.views.generic import ListView, DetailView
 # Model 
 from AppShop.models import Product
 
+# Mixin
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 # Create your views here.
 
 class Home(ListView):
     model = Product
     template_name = "home.html"
+
+class ProductDetail (LoginRequiredMixin, DetailView):
+    model = Product
+    template_name = 'product_detail.html'   
 
